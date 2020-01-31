e88d374c9884017a98a7347c58514c05a98f7288
Winston <winston.barlow@code.berlin>
2020-01-31 18:32:46 +0100

    adding first operations

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+property_price = float(input('Enter property price in £GBP: '))
+ltv = float(input('Enter the maximum LTV offered by your bank: '))
+
+loan_amount = property_price*(ltv/100)
+
+print("The loan amount is: = " + str(property_price*(ltv/100)))
+print("The deposit amount is: = " + str(property_price*(1-(ltv/100))))
+
+deposit = float(input('Enter amount of deposit: '))
+
+new_ltv = (1-(deposit/property_price))*100
+
+print("The new LTV would be of: = " + str(new_ltv) + str('%'))
